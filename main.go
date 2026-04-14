f46ca660643d7ae1e51da9ecdb60157fc5a0ce06
David Saah <davesaah@gmail.com>
2026-04-14 13:29:32 +0000

    attach platform to localhost as mirrors

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 	"sync"
@@ -15,18 +16,30 @@ type Platform struct {
 	url   string
 	name  string
 	token string
+	host  string
 }
 
 type Mirror struct {
 	platforms []Platform
 }
 
-func (m *Mirror) add(name, token, url string) {
+func (m *Mirror) add(name, token, _url string) error {
+	urlObj, err := url.Parse(_url)
+	if err != nil {
+		return fmt.Errorf("invalid mirror url: %w", err)
+	}
+
+	hostParts := strings.Split(urlObj.Hostname(), ".")
+	tld := hostParts[len(hostParts)-1]
+
 	m.platforms = append(m.platforms, Platform{
-		url:   url,
+		url:   _url,
 		name:  name,
 		token: token,
+		host:  fmt.Sprintf("%s.%s", name, tld),
 	})
+
+	return nil
 }
 
 func getTokens() (map[string]string, error) {
@@ -34,7 +47,7 @@ func getTokens() (map[string]string, error) {
 
 	f, err := os.Open(".env")
 	if err != nil {
-		return nil, fmt.Errorf("env file not found: %w", err)
+		return nil, fmt.Errorf("unable to get tokens. env file not found: %w", err)
 	}
 
 	scanner := bufio.NewScanner(f)
@@ -99,6 +112,56 @@ func (p *Platform) createRepo(data RepoData) error {
 	return nil
 }
 
+func (p *Platform) sync(localOwner, repoName, localToken string) error {
+	fmt.Printf("[*] Adding %s mirror...\n", p.name)
+
+	user := EXTERNAL_USER
+	if p.name == "gitlab" {
+		user = "oauth2"
+	}
+
+	endpoint := fmt.Sprintf("%s/repos/%s/%s/push_mirrors", LOCAL_URL, localOwner, repoName)
+	remoteUrl := fmt.Sprintf("https://%s:%s@%s/%s/%s.git", user, p.token, p.host, EXTERNAL_USER, repoName)
+
+	_payload := struct {
+		RemoteAddress string `json:"remote_address"`
+		SyncOnCommit  bool   `json:"sync_on_commit"`
+		Interval      string `json:"interval"`
+	}{
+		RemoteAddress: remoteUrl,
+		SyncOnCommit:  false,
+		Interval:      "24h",
+	}
+	payload, err := json.Marshal(_payload)
+	if err != nil {
+		return fmt.Errorf("unable to parse payload for syncing mirror: %w", err)
+	}
+
+	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(payload))
+	if err != nil {
+		return fmt.Errorf("unable to create http request object: %w", err)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("token %s", localToken))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("http request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// body, _ := io.ReadAll(resp.Body)
+	// fmt.Printf("[%s] status=%d body=%s\n", p.name, resp.StatusCode, string(body))
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("[!] failed to add %s mirror\n", p.name)
+	}
+
+	fmt.Printf("[->] %s mirror added successfully\n", p.name)
+	return nil
+}
+
 const LOCAL_URL = "https://git.davesaah-pc/api/v1"
 const EXTERNAL_USER = "davesaah"
 const VERIFY_CERT = "/etc/ssl/homelab/git.davesaah-pc.pem"
@@ -107,7 +170,7 @@ func main() {
 	var err error
 
 	repoName := "testing-mirror-sync-v2"
-	// localOwner := ""
+	localOwner := "davesaah"
 	visibility := "private"
 
 	var wg sync.WaitGroup
@@ -152,9 +215,11 @@ func main() {
 		}
 
 		wg.Go(func() {
-			if err := p.createRepo(d); err != nil {
-				fmt.Println(err)
-			}
+			err = p.createRepo(d)
+			check(WithErr(err))
+
+			err = p.sync(localOwner, repoName, tokens["LOCALHOST_TOKEN"])
+			check(WithErr(err))
 		})
 	}
 
